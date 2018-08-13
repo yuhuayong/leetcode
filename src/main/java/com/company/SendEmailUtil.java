87e83cfcb381b846d0f5de6ab1ec01ab75a9c49d
“your_u <1657202884@qq.com>
2018-08-13 10:25:01 +0800

    2018.8.13-加方法

---
+++ src/main/java/com/company/SendEmailUtil.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by yuhy on 2018/8/13
+ * sendEmail 发件人地址
+ * sendEmailPwd 授权码代替密码（更安全）  授权码的获取：进入个人邮箱，点击设置->账户，SMTP服务选项
+ * title 邮件标题
+ * content 邮件正文
+ * toEmailAddress 收件人地址
+ */
+public class SendEmailUtil {
+    public static void sendMail(String sendMail,String sendEmailPwd,String title,String content,String[] toEmailAddress)throws Exception{
+
+    }
+}
